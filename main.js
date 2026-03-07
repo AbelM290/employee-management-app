b2f28ac0c9a12fa23e42d4f3a1d03d1a50c0ccfb
AbelM290 <abelmumo290@gmail.com>
2026-03-07 03:31:21 -0800

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+function editalert()
+{
+    alert("Edit mode is activated!");
+}
+function confirmDelete()
+{
+    let result = confirm("Are you sure you want to delete?");
+    if(result)
+    {alert("record deleted");}
+}
